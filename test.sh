a4035b894bc7945e689c0a576bd44c4ebaf63cd4
Louis Dionne <louis.dionne92@gmail.com>
2013-03-24 15:04:44 -0400

    Tweak the test script some more.

--- test.sh
+++ test.sh
@@ -1,47 +1,54 @@
 #!/bin/sh -e
 
+time_file=.____time_file218h31u2hekqjn89dsa
+
 run_test() {
-  local file=$1
+  local file="$1"
   local algorithms=("${!2}")
-  local reference=$3
-  local print_stats=${4-false}
+  local reference="$3"
 
-  local num_vertices=$(sed -n -E -e '/^  [0-9]+;$/p' $file | wc -l)
-  local adj_list=$(sed -n -E -e 's/^  ([0-9]) -> ([0-9]);$/\1,\2/p' $file)
-  local num_edges=$(echo "$adj_list" | wc -w)
+  local num_vertices=$(sed -n -E '/^ *[0-9]+ *; *$/p' "$file" | wc -l | tr -d ' ')
+  local adj_list=$(sed -n -E 's/^ *([0-9]+) *-> *([0-9]+) *; *$/\1,\2/p' "$file")
+  local num_edges=$(echo "$adj_list" | wc -w | tr -d ' ')
 
-  local expected_cycles=$($reference $num_vertices $adj_list)
-  local expected_number_of_cycles=$(echo "$expected_cycles" | wc -l)
+  count_cycles() { echo "$1" | wc -l | tr -d ' '; }
 
-  if $print_stats; then
-    echo "$num_vertices vertices"
-    echo "$num_edges edges"
-  fi
+  local expected_cycles=$({ time $reference "$num_vertices" $adj_list; } 2>"$time_file")
+  local time_taken=$(cat "$time_file")
+  local expected_number_of_cycles=$(count_cycles "$expected_cycles")
+
+  echo "testing graph in $file with $num_vertices vertices and $num_edges edges"
+  echo "the reference algorithm ($reference) got:"
+  echo "$expected_number_of_cycles cycles"
+  echo "$time_taken"
+  echo
 
   for algo in "${algorithms[@]}"; do
-    local actual_cycles=$($algo $num_vertices $adj_list)
-    local actual_number_of_cycles=$(echo "$actual_cycles" | wc -l)
+    local actual_cycles=$({ time $algo "$num_vertices" $adj_list; } 2>"$time_file")
+    local time_taken=$(cat "$time_file")
+    local actual_number_of_cycles=$(count_cycles "$actual_cycles")
+
+    echo "$algo got:"
+    echo "$actual_number_of_cycles cycles"
+    echo "$time_taken"
+    echo
 
     if [ "$expected_cycles" != "$actual_cycles" ]; then
       echo "error: $algo differs from the reference"
 
+      echo "expected cycles (as computed by $reference):"
       echo "---------------------------------------------"
-      echo "expected results (as computed by $reference):"
-      echo "expected $expected_number_of_cycles cycles"
       echo "$expected_cycles"
       echo "---------------------------------------------"
 
+      echo "actual cycles (as computed by $algo):"
       echo "---------------------------------------------"
-      echo "actual results (as computed by $algo):"
-      echo "got $actual_number_of_cycles cycles"
       echo "$actual_cycles"
       echo "---------------------------------------------"
 
       exit 1
     fi
   done
-
-  echo "$file finished, found $expected_number_of_cycles cycles"
 }
 
 algorithms=(
@@ -53,7 +60,7 @@ algorithms=(
   "./abate/cycles_functional.native"
 )
 
-reference_algorithm=./hawick/circuits_hawick
+reference_algorithm="./hawick/circuits_hawick"
 
 
 echo compiling ./hawick/circuits_hawick...
@@ -73,8 +80,10 @@ echo testing graphs...
 
 counter=0
 for file in *.dot; do
-  if run_test $file algorithms[@] $reference_algorithm; then
+  if run_test "$file" algorithms[@] "$reference_algorithm"; then
+    echo "------------------------------------------------------------------"
     counter=$((counter+1))
   fi
 done
+rm "$time_file"
 echo successfully tested $counter graphs
